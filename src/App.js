937e7ad9328d0210a88fba5c1da20522198c0bf9
Raghav <chughraghav@gmail.com>
2019-07-10 20:31:50 +0530

    Signup and Auth route

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import jwtDecode from "jwt-decode";
 //Redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { SET_AUTHENTICATED } from './redux/types';
+import { logoutUser, getUserData } from './redux/action/userActions';
 //Components
 import Navbar from "./components/Navbar";
 import AuthRoute from "./util/AuthRoute";
@@ -16,19 +18,23 @@ import AuthRoute from "./util/AuthRoute";
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
+import axios from "axios";
 
 const theme = createMuiTheme(themeFile);
 
-let authenticated;
+
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   console.log(decodedToken);
   if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logoutUser())
     window.location.href = "/login";
-    authenticated = false;
+    
   } else {
-    authenticated = true;
+    store.dispatch({ type: SET_AUTHENTICATED});
+    axios.defaults.headers.common['Authorization'] = token;
+    store.dispatch(getUserData());
   }
 }
 
@@ -45,13 +51,13 @@ function App() {
                 exact
                 path="/login"
                 component={login}
-                authenticated={authenticated}
+                
               />
               <AuthRoute
                 exact
                 path="/signup"
                 component={signup}
-                authenticated={authenticated}
+              
               />
             </Switch>
           </div>
