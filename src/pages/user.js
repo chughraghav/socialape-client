e240b29e7a427c771c54deb6f51057ed7fa982bd
Raghav <chughraghav@gmail.com>
2019-07-15 00:40:59 +0530

    Add Skeletons

--- src/pages/user.js
+++ src/pages/user.js
@@ -5,8 +5,11 @@ import Scream from "../components/scream/Scream";
 import StaticProfile from '../components/profile/StaticProfile.js';
 import Grid from "@material-ui/core/Grid";
 
+
 import { connect } from "react-redux";
 import { getUserData } from "../redux/action/dataActions";
+import ScreamSkeleton from "../util/ScreamSkeleton";
+import ProfileSkeleton from '../util/ProfileSkeleton'
 
 class user extends Component {
   state = {
@@ -34,7 +37,7 @@ class user extends Component {
     const {screamIdParam} = this.state; 
 
     const screamsMarkup = !loading ? (
-        <p>Loading data...</p>
+      <ScreamSkeleton />
     ) : screams === null ? (
         <p>No Screams from this user</p>
     ) : !screamIdParam? (
@@ -53,7 +56,7 @@ class user extends Component {
         </Grid>
         <Grid item sm={4} xs={12}>
           {this.state.profile === null ? (
-              <p>Loading Profile...</p>
+              <ProfileSkeleton />
           ) : (
             <StaticProfile profile={this.state.profile} />
           ) }
