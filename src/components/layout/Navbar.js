30863255c956994c7a84a1840a10f9b2f76b9fca
Raghav <chughraghav@gmail.com>
2019-07-14 23:56:25 +0530

    Add Notifications

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,10 +5,10 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import MyButton from "../../util/MyButton";
 import PostScream from '../scream/PostScream';
+import Notifications from './Notifications';
 
 //Icons
 import HomeIcon from "@material-ui/icons/Home";
-import Notifications from "@material-ui/icons/Notifications";
 
 //MUI stuff
 import AppBar from "@material-ui/core/AppBar";
@@ -29,9 +29,7 @@ class Navbar extends Component {
                   <HomeIcon />
                 </MyButton>
               </Link>
-              <MyButton tip="Notifications">
                 <Notifications />
-              </MyButton>
             </Fragment>
           ) : (
             <Fragment>
