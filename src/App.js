387ca67478dd063b3e935a935cd72a32a38b1e9b
Raghav <chughraghav@gmail.com>
2019-07-14 21:48:05 +0530

    Add user page

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import jwtDecode from "jwt-decode";
 //Redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { SET_AUTHENTICATED } from './redux/types';
-import { logoutUser, getUserData } from './redux/action/userActions';
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logoutUser, getUserData } from "./redux/action/userActions";
 //Components
 import Navbar from "./components/layout/Navbar";
 import AuthRoute from "./util/AuthRoute";
@@ -18,22 +18,21 @@ import AuthRoute from "./util/AuthRoute";
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
+import user from "./pages/user";
 import axios from "axios";
 
 const theme = createMuiTheme(themeFile);
-
 
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   console.log(decodedToken);
   if (decodedToken.exp * 1000 < Date.now()) {
-    store.dispatch(logoutUser())
+    store.dispatch(logoutUser());
     window.location.href = "/login";
-    
   } else {
-    store.dispatch({ type: SET_AUTHENTICATED});
-    axios.defaults.headers.common['Authorization'] = token;
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common["Authorization"] = token;
     store.dispatch(getUserData());
   }
 }
@@ -47,18 +46,9 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={home} />
-              <AuthRoute
-                exact
-                path="/login"
-                component={login}
-                
-              />
-              <AuthRoute
-                exact
-                path="/signup"
-                component={signup}
-              
-              />
+              <AuthRoute exact path="/login" component={login} />
+              <AuthRoute exact path="/signup" component={signup} />
+              <Route exact path="/users/:handle" component={user} />
             </Switch>
           </div>
         </Router>
