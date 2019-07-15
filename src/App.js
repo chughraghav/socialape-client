b3048d2bf2cf43631890464a2d6080e4285d24b0
Raghav <chughraghav@gmail.com>
2019-07-15 20:05:14 +0530

    eevert "Firebase deploy"
    This reverts commit 4fe479187b40b57628d6bb537f13720cc7c7822f.

--- src/App.js
+++ src/App.js
@@ -23,9 +23,6 @@ import axios from "axios";
 
 const theme = createMuiTheme(themeFile);
 
-axios.defaults.baseURL =
-  "https://asia-east2-socialape-20235.cloudfunctions.net/api";
-
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
@@ -52,11 +49,7 @@ function App() {
               <AuthRoute exact path="/login" component={login} />
               <AuthRoute exact path="/signup" component={signup} />
               <Route exact path="/users/:handle" component={user} />
-              <Route
-                exact
-                path="/users/:handle/scream/:screamId"
-                component={user}
-              />
+              <Route exact path="/users/:handle/scream/:screamId" component={user} />
             </Switch>
           </div>
         </Router>
